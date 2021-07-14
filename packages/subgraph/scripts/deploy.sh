baf9baeac4433ace0424fef34f040f4fcdc1fb3b
lkndjrt <lkndjrt@gmail.com>
2021-07-14 18:31:01 -0300

    subgraph: rename subgraph to mimic

--- packages/subgraph/scripts/deploy.sh
+++ packages/subgraph/scripts/deploy.sh
@@ -23,13 +23,13 @@ fi
 
 # Create subgraph if missing
 {
-  graph create mimic-fi/core-${NETWORK} --node ${GRAPH_NODE}
+  graph create mimic-fi/${NETWORK} --node ${GRAPH_NODE}
 } || {
   echo 'Subgraph was already created'
 }
 
 # Deploy subgraph
-graph deploy mimic-fi/core-${NETWORK} \
+graph deploy mimic-fi/${NETWORK} \
   --ipfs ${IPFS_NODE} \
   --node ${GRAPH_NODE} \
   --access-token "$GRAPHKEY"
