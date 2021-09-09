fa24b99757f0d606f20e97f579325f528d1bb6e0
lkndjrt <lkndjrt@gmail.com>
2021-09-09 16:53:33 -0300

    vault: add hardhat local config plugin

--- packages/vault/hardhat.config.ts
+++ packages/vault/hardhat.config.ts
@@ -1,13 +1,17 @@
+import path from 'path'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import 'hardhat-local-networks-config-plugin'
 
 import { task } from 'hardhat/config'
+import { homedir } from 'os'
 import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names'
 import { overrideFunctions } from '@mimic-fi/v1-helpers'
 
 task(TASK_COMPILE).setAction(overrideFunctions(['query']))
 
 export default {
+  localNetworksConfig: path.join(homedir(), '/.hardhat/networks.mimic.json'),
   solidity: {
     version: '0.8.0',
     settings: {
