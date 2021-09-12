3df4e300ab9d71c1386b5be8657bebe9d46b2aa4
lkndjrt <lkndjrt@gmail.com>
2021-09-12 20:59:57 -0300

    agreement: add local hardhat config

--- packages/agreements/hardhat.config.ts
+++ packages/agreements/hardhat.config.ts
@@ -1,7 +1,12 @@
+import path from 'path'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
+import 'hardhat-local-networks-config-plugin'
+
+import { homedir } from 'os'
 
 export default {
+  localNetworksConfig: path.join(homedir(), '/.hardhat/networks.mimic.json'),
   solidity: {
     version: '0.8.0',
     settings: {
