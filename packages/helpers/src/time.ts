b8146fc5d9a0dbdbfffad384e0591a2a6018ea4e
lkndjrt <lkndjrt@gmail.com>
2022-05-30 11:26:12 -0300

    helpers: fix hardhat imports in time utils

--- packages/helpers/src/time.ts
+++ packages/helpers/src/time.ts
@@ -1,5 +1,4 @@
 import { BigNumber } from 'ethers'
-import { ethers, network } from 'hardhat'
 
 import { BigNumberish, bn } from './numbers'
 
@@ -11,15 +10,18 @@ export const WEEK = DAY * 7
 export const MONTH = DAY * 30
 
 export const currentTimestamp = async (): Promise<BigNumber> => {
+  const { network } = await import('hardhat')
   const { timestamp } = await network.provider.send('eth_getBlockByNumber', ['latest', true])
   return bn(timestamp)
 }
 
 export const advanceTime = async (seconds: BigNumberish): Promise<void> => {
+  const { ethers } = await import('hardhat')
   await ethers.provider.send('evm_increaseTime', [parseInt(seconds.toString())])
   await ethers.provider.send('evm_mine', [])
 }
 
 export const setNextBlockTimestamp = async (timestamp: BigNumberish): Promise<void> => {
+  const { ethers } = await import('hardhat')
   await ethers.provider.send('evm_setNextBlockTimestamp', [parseInt(timestamp.toString())])
 }
