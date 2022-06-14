5c374b83c2370960fece6eb11f6b292fabd04b15
lkndjrt <lkndjrt@gmail.com>
2022-06-14 17:11:33 -0300

    subgraph: improve linter rules

--- packages/subgraph/.eslintrc.js
+++ packages/subgraph/.eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   rules: {
-    "semi": "off",
+    "semi": ["error", "never"],
     "max-len": ["error", { code: 120 }],
     "no-unused-vars": ["error"],
+    "no-unexpected-multiline": "error",
     "comma-spacing": ["error", { before: false, after: true }]
   },
 };
